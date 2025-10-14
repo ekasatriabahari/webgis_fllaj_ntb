7ebb20414baa75b65376658ea83bbec9e078970f
ekasatriabahari <ekasatriabahari@outlook.com>
2025-10-14 13:56:16 +0800

    draft table rencana

--- app/Views/pages/landing/index.php
+++ app/Views/pages/landing/index.php
@@ -167,7 +167,7 @@
                                 <a class="nav-link" href="#tableKondisi">Fasilitas</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                                <a class="nav-link" href="#tableRencana">Rencana</a>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -273,6 +273,29 @@
                                 </thead>
                                 <tbody>
                                 </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5><i class="fas fa-table"></i> Tabel Rencana</h5>
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered table-striped" id="tableRencana">
+                                <thead>
+                                    <tr>
+                                        <th width="5%">No</th>
+                                        <th>Jenis Fasilitas</th>
+                                        <th>Nama Fasilitas</th>
+                                        <th>Koordinat</th>
+                                        <th>Tahun Survey</th>
+                                        <th>Detail</th>
+                                    </tr>
+                                </thead>
                             </table>
                         </div>
                     </div>
